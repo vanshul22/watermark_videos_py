5f24c31bf57ab8bde8b1672554c897d418aa46d5
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-11-02 17:30:31 +0530

    Some changes done.

--- watermark.py
+++ watermark.py
@@ -1,11 +1,13 @@
 from moviepy.editor import *
 
-video_clip = VideoFileClip("Future_Generali_Editable_Video.mp4").subclip(0, 5)
+video_clip = VideoFileClip("Future_Generali_Editable_Video.mp4", audio=True)
 
-txt_clip = TextClip("Vanshul", fontsize=30, color="black").set_duration(5)
+video_clip = video_clip.subclip(0, 5)
 
-txt_clip = txt_clip.set_position("bottom","right")
+txt_clip = TextClip("Vanshul", fontsize=30, color="black")
+
+txt_clip = txt_clip.set_pos("bottom", 30).set_duration(5)
 
 video = CompositeVideoClip([video_clip, txt_clip])
 
-video.write_videofile("test.mp4")
+video.write_videofile("text_add_result.mov", fps=24)
