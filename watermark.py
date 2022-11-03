6f929926a04238b51289e3f61576b0540f7f9942
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-11-03 10:57:16 +0530

    Fixed some erroers and project is working.

--- watermark.py
+++ watermark.py
@@ -1,13 +1,20 @@
+# Import everything needed to edit video clips
 from moviepy.editor import *
 
-video_clip = VideoFileClip("Future_Generali_Editable_Video.mp4", audio=True)
+# Load myHolidays.mp4 and select the subclip 00:00:00 - 00:00:05
+video_clip = VideoFileClip("Future_Generali_Editable_Video.mp4").subclip(0, 5)
 
-video_clip = video_clip.subclip(0, 5)
+# Reduce the audio volume (volume x 0.8)
+# video_clip = clip.volumex(0.8)
 
-txt_clip = TextClip("Vanshul", fontsize=30, color="black")
+# Generate a text clip. You can customize the font, color, etc.
+txt_clip = TextClip("Keval - +919892580676", fontsize=70, color='black')
 
-txt_clip = txt_clip.set_pos("bottom", 30).set_duration(5)
+# Say that you want it to appear 5s at the center of the screen
+txt_clip = txt_clip.set_pos('center').set_duration(5)
 
+# Overlay the text clip on the first video clip
 video = CompositeVideoClip([video_clip, txt_clip])
 
-video.write_videofile("text_add_result.mov", fps=24)
+# Write the result to a file (many options available !)
+video.write_videofile('text_add_result.mov', fps=24, codec='libx264')
